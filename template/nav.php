8c44d0a8a30b97a75ddf7e53a2af3e41169dc748
Rahul Yadav <93183204+Rahuly007@users.noreply.github.com>
2021-10-26 13:34:18 +0530

    Add files via upload

---
+++ template/nav.php
@@ -0,0 +1,84 @@
+<div class=" sidebar" role="navigation">
+    <div class="navbar-collapse">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+            <ul class="nav" id="side-menu">
+                <li>
+                    <a href="index.php" class="active"><i class="fa fa-home nav_icon"></i>Dashboard</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-cogs nav_icon"></i>Components <span class="nav-badge">12</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li>
+                            <a href="grids.php">Grid System</a>
+                        </li>
+                        <li>
+                            <a href="media.php">Media Objects</a>
+                        </li>
+                    </ul>
+                    <!-- /nav-second-level -->
+                </li>
+                <li class="">
+                    <a href="#"><i class="fa fa-book nav_icon"></i>UI Elements <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li>
+                            <a href="general.php">General<span class="nav-badge-btm">08</span></a>
+                        </li>
+                        <li>
+                            <a href="typography.php">Typography</a>
+                        </li>
+                    </ul>
+                    <!-- /nav-second-level -->
+                </li>
+                <li>
+                    <a href="widgets.php"><i class="fa fa-th-large nav_icon"></i>Widgets <span class="nav-badge-btm">08</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-envelope nav_icon"></i>Mailbox<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li>
+                            <a href="inbox.php">Inbox <span class="nav-badge-btm">05</span></a>
+                        </li>
+                        <li>
+                            <a href="compose.php">Compose email</a>
+                        </li>
+                    </ul>
+                    <!-- //nav-second-level -->
+                </li>
+                <li>
+                    <a href="index.php?page=tables"><i class="fa fa-table nav_icon"></i>Tables <span class="nav-badge">05</span></a>
+                </li>
+                <li>
+                    <a href="index.php?page=form"><i class="fa fa-check-square-o nav_icon"></i>Forms<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li>
+                            <a href="index.php?page=form" action="<?php echo $_SERVER['PHP_SELF']; ?>">Basic Forms <span class="nav-badge-btm">07</span></a>
+                        </li>
+                        <li>
+                            <a href="validation.php">Validation</a>
+                        </li>
+                    </ul>
+                    <!-- //nav-second-level -->
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-file-text-o nav_icon"></i>Pages<span class="nav-badge-btm">02</span><span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li>
+                            <a href="login.php">Login</a>
+                        </li>
+                        <li>
+                            <a href="signup.php">SignUp</a>
+                        </li>
+                        <li>
+                            <a href="blank-page.php">Blank Page</a>
+                        </li>
+                    </ul>
+                    <!-- //nav-second-level -->
+                </li>
+                <li>
+                    <a href="charts.php" class="chart-nav"><i class="fa fa-bar-chart nav_icon"></i>Charts <span class="nav-badge-btm pull-right">new</span></a>
+                </li>
+            </ul>
+            <!-- //sidebar-collapse -->
+        </nav>
+    </div>
+</div>
